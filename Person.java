1d3dc3383b8a5f5f8e8740dc495030187b4f075a
Hanleigh <hanleighrose@gmail.com>
2016-10-28 17:09:39 -0500

    Updated Person.java

--- Person.java
+++ Person.java
@@ -1,1 +1,12 @@
-public class Person{}
+public class Person{
+	private String lastName;
+	private String firstName;
+	private String zipCode;
+
+	//default constructor
+	private void Person(){
+		lastName = "X";
+		firstName = "X";
+		zipCode = "X";
+	}
+}
