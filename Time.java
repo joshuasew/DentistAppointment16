526fb69c95416b2461c32aef04d92c6799da1639
Joshua <jysewhee@mail.lipscomb.edu>
2016-10-28 17:26:42 -0500

    updated Time.java

--- Time.java
+++ Time.java
@@ -1,3 +1,14 @@
 public class Time{
+  private int hours;
+  private int minutes;
 
+  private void displayTime()
+  {
+    System.out.printf("%d:%.2d", hours, minutes);
+  }
+
+  private void getTime(int minutes)
+  {
+    newTime = minutes
+  }
 }
