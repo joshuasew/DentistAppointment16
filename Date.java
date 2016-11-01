e4b27035d671eec6624ee55535fa14f385f261b0
Hanleigh <hanleighrose@gmail.com>
2016-10-31 20:29:24 -0500

    Updated Date.java

--- Date.java
+++ Date.java
@@ -9,34 +9,33 @@
    private int day;
    private int year;
 
-   private void getDate()
+   public Date()
    {
      month = 1;
      day = 1;
      year = 2000;
    }
 
-   private void getDate(int month1, int day1, int year1){
+   public Date(int month1, int day1, int year1){
      month = month1;
      day = day1;
      year = year1;
    }
 
-   private void displayDate()
+   public void displayDate()
    {
      //System.out.printf("%d/%d/%d", month, day, year);
      System.out.print(month + "/" + day + "/" + year);
    }
 
    public static void main(String args[]){
-     Date myDate = new Date();
+   /*Date myDate = new Date();
      if (args.length == 0){
        myDate.getDate();
        myDate.displayDate();
      }
      if (args.length == 3){
      myDate.getDate(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-     myDate.displayDate();
+     myDate.displayDate();*/
    }
  }
-}
